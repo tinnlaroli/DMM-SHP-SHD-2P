09a13a4e07f9904cdeda83479530b18d983f3e16
Bernardo1937 <20233L001143@utcv.edu.mx>
2025-03-18 13:30:32 -0600

    inicio de proyecto y creacion de la pagina de perfiles

---
+++ seccion_aceptacion/src/app/pages/profiles/profiles.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profiles',
+  templateUrl: './profiles.page.html',
+  styleUrls: ['./profiles.page.scss'],
+  standalone: false,
+})
+export class ProfilesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
