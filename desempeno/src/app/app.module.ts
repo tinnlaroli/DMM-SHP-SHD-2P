1594ccf7a010d0a6e756e5e5c63650567b9fa710
Ferchiisp <20233l001037@utcv.edu.mx>
2025-03-18 21:36:09 -0600

    feat: agrgacion de importaciones a app.module.ts

--- desempeno/src/app/app.module.ts
+++ desempeno/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SplashComponent } from './splash/splash.component'; // Agrega esta línea
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent,
+    SplashComponent // Agrega SplashComponent aquí
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
