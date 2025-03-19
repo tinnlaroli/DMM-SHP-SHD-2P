5e05fef3e2cc01c63ce7864da289a6a791680d67
Ferchiisp <20233l001037@utcv.edu.mx>
2025-03-18 21:33:39 -0600

    feat: codigo de splash.components.ts

---
+++ desempeno/src/app/splash/splash.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.component.html',
+  styleUrls: ['./splash.component.scss'],
+  standalone: false
+})
+export class SplashComponent  implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 3000); // 3000 milisegundos = 3 segundos
+  }
+
+}
